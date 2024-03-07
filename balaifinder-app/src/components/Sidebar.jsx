a59a6d29ec9dc4ee417d03a83bb2814bd23b0e44
CodingwithJosh/ <joshua.laurence.fabi@gmail.com>
2024-03-07 17:54:44 +0800

    update the sidebar

--- balaifinder-app/src/components/Sidebar.jsx
+++ balaifinder-app/src/components/Sidebar.jsx
@@ -1,46 +1,84 @@
-import {FaHome} from 'react-icons/fa'
+import { BsHouseGearFill } from "react-icons/bs"    
+import { RiDashboardFill } from "react-icons/ri"
+import { BiSolidMessageDetail } from "react-icons/bi"
+import { FaClipboardList } from "react-icons/fa"
+import { IoMdSettings } from "react-icons/io"
+import { IoMdArrowDropdown } from "react-icons/io"
+import React, { useState, useRef, useEffect } from 'react'
+export default function Sidebar() {
+    const [isDropdownVisible, setIsDropdownVisible] = useState(false)  
+    const toggleDropdown = () => {
+      setIsDropdownVisible(!isDropdownVisible)
+    };
+  
 
-
-
-export default function Sidebar(){
-    return(
-        <div className="w-64 bg-gray-400 fixed h-full px-4 py-2">
-            <div className='my-2 mb-4'>
-                <h1 className="text-2xl text-black font-bold">Realtor Dashboard</h1>
-            </div>
-            <hr />
-            <ul className='mt-5 text-black font-bold'>
-                <li className='mb-2 rounded hover:shadow hover:bg-sky-500 py-2'>
-                    <a href="" className='px-3'>
-                        <FaHome className='inline-block size-6 mr-2 -mt-2'></FaHome>
-                        Dashboard
+  return (
+    <div className="w-64 bg-whitesmoke fixed h-full px-4 py-2 shadow">
+      <div className="my-2 mb-4">
+        <h1 className="text-2xl text-black font-bold">Realtor Dashboard</h1>
+      </div>
+      <hr />
+      <ul className="mt-5 text-black font-semibold items-center justify-center">
+        <li className="mb-2 rounded hover:shadow hover:bg-sky-100 py-2">
+          <a href="" className="px-3">
+            <RiDashboardFill className="inline-block size-6 mr-2 -mt-2" />
+            Dashboard
+          </a>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-sky-100 py-2">
+            <button className="px-3" onClick={toggleDropdown}>
+                <BsHouseGearFill className="inline-block size-6 mr-2 -mt-2" />
+                Manage Property
+                <IoMdArrowDropdown className="inline-block size-6 ml-2 -mt-0.5" />
+            </button>
+            <ul
+                className={`justify-center mt-2 h-auto ${isDropdownVisible ? '' : 'hidden'}`}>
+                <li>
+                    <a
+                    href=""
+                    className="block text-sm px-4 py-2 text-gray-800 hover:bg-white"
+                    >
+                    Add Property
                     </a>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-sky-500 py-2'>
-                    <a href="" className='px-3'>
-                        <FaHome className='inline-block size-6 mr-2 -mt-2'></FaHome>
-                        Dashboard
+                <li>
+                    <a
+                    href=""
+                    className="block text-sm px-4 py-2 text-gray-800 hover:bg-white"
+                    >
+                    Delete Property
                     </a>
                 </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-sky-500 py-2'>
-                    <a href="" className='px-3'>
-                        <FaHome className='inline-block size-6 mr-2 -mt-2'></FaHome>
-                        Dashboard
-                    </a>
-                </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-sky-500 py-2'>
-                    <a href="" className='px-3'>
-                        <FaHome className='inline-block size-6 mr-2 -mt-2'></FaHome>
-                        Dashboard
-                    </a>
-                </li>
-                <li className='mb-2 rounded hover:shadow hover:bg-sky-500 py-2'>
-                    <a href="" className='px-3'>
-                        <FaHome className='inline-block size-6 mr-2 -mt-2'></FaHome>
-                        Dashboard
+                <li>
+                    <a
+                    href=""
+                    className="block text-sm px-4 py-2 text-gray-800 hover:bg-white"
+                    >
+                    Update Property
                     </a>
                 </li>
             </ul>
-        </div>
-    )
+        </li>
+
+        <li className="mb-2 rounded hover:shadow hover:bg-sky-100 py-2">
+          <a href="" className="px-3">
+            <FaClipboardList className="inline-block size-6 mr-2 -mt-2" />
+            Property Lists
+          </a>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-sky-100 py-2">
+          <a href="" className="px-3">
+            <BiSolidMessageDetail className="inline-block size-6 mr-2 -mt-2" />
+            Messages
+          </a>
+        </li>
+        <li className="mb-2 rounded hover:shadow hover:bg-sky-100 py-2">
+          <a href="" className="px-3">
+            <IoMdSettings className="inline-block size-6 mr-2 -mt-2" />
+            Settings
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
