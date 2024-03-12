9a1ea174ab0dca6a5ad7348892038680a54df5a0
CodingwithJosh/ <joshua.laurence.fabi@gmail.com>
2024-03-12 18:02:33 +0800

    updates

--- balaifinder-app/src/pages/Home.jsx
+++ balaifinder-app/src/pages/Home.jsx
@@ -1,7 +1,5 @@
-import { FaArrowRight } from 'react-icons/fa'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import Register from './Register'
 import Hero from '../components/Hero'
 import Offers from '../components/Offers'
 import Choose from '../components/Choose'
