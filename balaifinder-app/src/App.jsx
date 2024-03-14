3bde77414c1224d6834f5deab9366d4263ec7187
AluLicup <licup.alryan.bscs@gmail.com>
2024-03-14 12:29:20 +0800

    login/register(bcrypt,jwt,cookies)

--- balaifinder-app/src/App.jsx
+++ balaifinder-app/src/App.jsx
@@ -8,18 +8,31 @@ import Maintenance from './pages/Maintenance'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import { useContext } from 'react'
+import { AuthContext } from './context/authContext'
+import { Navigate } from 'react-router-dom'
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   return (
     <div className='App'>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/properties" element={<Properties />} />
-            <Route path="/matching" element={<Maintenance />} />
+            <Route path="/matching" element={<ProtectedRoute> <Maintenance /> </ProtectedRoute>} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
         </Routes>
     </div>
   )
